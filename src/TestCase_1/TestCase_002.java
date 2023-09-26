f0bef7de85d30602fbee8ee7cf6fa2cce0858fad
Raill988 <139258857+Raill988@users.noreply.github.com>
2023-09-26 16:59:53 +0400

    Create Test Case 002 - frst steps

---
+++ src/TestCase_1/TestCase_002.java
@@ -0,0 +1,20 @@
+package TestCase_1;
+
+public class TestCase_002 {
+    public static void main(String[] args) {
+
+        /*
+        VCS enable
+        -> go to GitHub web
+        -> Creat new repository(give a name) in your repository
+        -> copy Link
+        -> come back IntelliJ -> Git option -> Manage Remotes-> add link and Create
+        -> Push the files
+         */
+
+
+
+
+
+    }
+}
